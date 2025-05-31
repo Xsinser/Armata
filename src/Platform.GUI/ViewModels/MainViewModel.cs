6f537ea85da18b71ab83520cc7c6e4076f849053
Xsinser <xsinser175@gmail.com>
2025-05-31 20:49:48 +0300

    Добавил возможность просмотра видео с камеры

--- src/Platform.GUI/ViewModels/MainViewModel.cs
+++ src/Platform.GUI/ViewModels/MainViewModel.cs
@@ -1,6 +1,43 @@
-﻿namespace Platform.GUI.ViewModels;
+﻿using Avalonia.Media.Imaging;
+using OpenCvSharp;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Platform.GUI.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
     public string Greeting => "Welcome to Avalonia!";
+
+
+    private Bitmap _bitmapCap;
+    public Bitmap BitmapCap
+    {
+        get => _bitmapCap;
+        set
+        {
+            _bitmapCap = value;
+            OnPropertyChanged();
+        }
+    }
+
+    VideoCapture _capture;
+
+    public MainViewModel()
+    {
+        _capture = new VideoCapture(0);
+        if (_capture.IsOpened())
+        {
+            Task.Run(() =>
+            {
+                while (_capture.IsOpened())
+                {
+                    // возможно нужно оптимизировать для уменьшения нагрузки цпу
+                    Mat mat = new Mat();
+                    _capture.Read(mat);
+                    BitmapCap = new Bitmap(mat.ToMemoryStream());
+                }
+            });
+        }
+    }
 }
--- src/Platform.GUI/Views/MainView.axaml.cs
+++ src/Platform.GUI/Views/MainView.axaml.cs
@@ -1,9 +1,13 @@
 ﻿using Avalonia.Controls;
+using OpenCvSharp;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Platform.GUI.Views;
 
 public partial class MainView : UserControl
 {
+
     public MainView()
     {
         InitializeComponent();
